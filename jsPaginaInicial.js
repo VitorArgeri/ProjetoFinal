2d09c98f846ad262c81fb4cdad325c3d6f993a9c
VitorArgeri <vitorargeri.tds2.senai@gmail.com>
2024-06-06 11:35:17 -0300

    Conclusão da Página Inicial e começo das outras páginas

---
+++ jsPaginaInicial.js
@@ -0,0 +1,29 @@
+var images = [
+    'Imagens/maverick.jpg',
+    'Imagens/Componentes.jpeg',
+    'Imagens/Carro.png',
+    'Imagens/Carro2.png'
+];
+
+var currentImageIndex = 0;
+
+function changeImageBefore() {
+    currentImageIndex--;
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1;
+    }
+    updateImage();
+}
+
+function changeImageAfter() {
+    currentImageIndex++;
+    if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
+    updateImage();
+}
+
+function updateImage() {
+    var img = document.getElementById('image');
+    img.src = images[currentImageIndex];
+}
